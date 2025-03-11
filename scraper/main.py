1491a35a7de876fb24723f703f45eb28d7c65893
Lobster <segdev07@gmail.com>
2025-03-10 21:19:25 -0400

    database update

--- scraper/main.py
+++ scraper/main.py
@@ -1,11 +1,13 @@
 import requests, json
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+ua = UserAgent()
 
 URL = "https://www.sekaipedia.org/wiki/Song_data_list"
 
 # Fake user agent so we can scrape the data
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+    "User-Agent": ua.random,
 }
 
 '''
